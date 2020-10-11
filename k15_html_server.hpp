cebd4e528dd4e21a9f67ab9d5f8b362bd51ecc5f
Felix <f.klinge15@gmail.com>
2020-10-11 20:57:30 +0200

    add & applied clang_format

--- k15_html_server.hpp
+++ k15_html_server.hpp
@@ -7,304 +7,291 @@
 
 namespace k15
 {
-    typedef int socketId;
+typedef int socketId;
 
-	struct html_server
-	{
-		socketId 			ipv4Socket;
-		socketId 			ipv6Socket; 
-		int 				port;
-	};
+struct html_server
+{
+    memory_allocator* pAllocator;
+    socketId          ipv4Socket;
+    socketId          ipv6Socket;
+    int               port;
+};
 
-    struct html_client
+struct html_client
+{
+    memory_allocator* pAllocator;
+    socketId          socket;
+};
+
+enum class request_method
+{
+    get,
+    post,
+    put,
+    del
+};
+
+struct html_request
+{
+    request_method method;
+    string_view    path;
+    slice< char >  message;
+};
+
+struct html_server_parameters
+{
+    memory_allocator* pAllocator;
+    int               port;
+    const char*       ipv4BindAddress;
+    const char*       ipv6BindAddress;
+};
+
+bool listenOnSocket( const socketId& socket, int protocol, int port, const char* bindAddress )
+{
+    if ( socket == INVALID_SOCKET )
     {
-        socketId socket;
+        return false;
+    }
+
+    sockaddr_in sockAddr;
+    sockAddr.sin_family      = protocol;
+    sockAddr.sin_port        = htons( port );
+    sockAddr.sin_addr.s_addr = inet_addr( bindAddress );
+
+    const int bindResult = bind( socket, ( const struct sockaddr* )&sockAddr, sizeof( sockAddr ) );
+    if ( bindResult == SOCKET_ERROR )
+    {
+        return false;
+    }
+
+    const int backlog      = 10; //FK: TODO: find reasonable number here
+    const int listenResult = listen( socket, backlog );
+
+    if ( listenResult == SOCKET_ERROR )
+    {
+        return false;
+    }
+
+    return true;
+}
+
+html_client* waitForClientConnection( html_server* pServer )
+{
+    fd_set readSockets;
+    FD_ZERO( &readSockets );
+    FD_SET( pServer->ipv4Socket, &readSockets );
+    FD_SET( pServer->ipv6Socket, &readSockets );
+
+    const int selectResult = select( 0, &readSockets, nullptr, nullptr, nullptr );
+    if ( selectResult == -1 )
+    {
+        return nullptr;
+    }
+
+    html_client* pClient = newObject< html_client >( pServer->pAllocator );
+    pClient->pAllocator  = pServer->pAllocator;
+    if ( FD_ISSET( pServer->ipv4Socket, &readSockets ) )
+    {
+        pClient->socket = accept( pServer->ipv4Socket, NULL, NULL );
+    }
+    else
+    {
+        pClient->socket = accept( pServer->ipv6Socket, NULL, NULL );
+    }
+
+    return pClient;
+}
+
+result< void > receiveClientData( slice< char >* pMessageBuffer, html_client* pClient )
+{
+    while ( true )
+    {
+        char      buffer[ 256 ];
+        const int bytesRead = recv( pClient->socket, buffer, 256, 0u );
+
+        if ( bytesRead == -1 )
+        {
+            return error_id::socket_error;
+        }
+        else if ( bytesRead == 0 )
+        {
+            pMessageBuffer->pushBack( '\0' );
+            return error_id::success;
+        }
+
+        char* pMessage = pMessageBuffer->pushBackRange( bytesRead );
+        if ( pMessage == nullptr )
+        {
+            return error_id::out_of_memory;
+        }
+
+        copyMemoryNonOverlapping( pMessage, bytesRead, buffer, bytesRead );
+
+        if ( bytesRead < sizeof( buffer ) )
+        {
+            pMessageBuffer->pushBack( '\0' );
+            return error_id::success;
+        }
+    }
+
+    return error_id::success;
+}
+
+result< html_request > parseHtmlRequest( slice< char >* pMessageBuffer )
+{
+    const char* pMessageRunningPtr = pMessageBuffer->getStart();
+    const char* pMessageEnd        = pMessageBuffer->getEnd();
+
+    enum class parse_state
+    {
+        method,
+        path,
+        finished
     };
 
-    enum class request_method
+    html_request request;
+    parse_state  state = parse_state::method;
+
+    while ( true )
     {
-        get,
-        post,
-        put,
-        del
-    };
-
-    struct html_request
-    {
-        request_method  method;
-        string_view     path;
-    };
-
-	struct html_server_parameters
-	{
-		int port;
-		const char* ipv4BindAddress;
-		const char* ipv6BindAddress;
-	};
-
-    bool listenOnSocket(const socketId& socket, int protocol, int port, const char* bindAddress)
-    {
-        if (socket == INVALID_SOCKET)
+        if ( pMessageRunningPtr == pMessageEnd )
         {
-            return false;
+            return error_id::parse_error;
         }
 
-        sockaddr_in sockAddr;
-        sockAddr.sin_family         = protocol;	
-        sockAddr.sin_port           = htons(port);
-        sockAddr.sin_addr.s_addr    = inet_addr(bindAddress);
-
-        const int bindResult = bind( socket, (const struct sockaddr*)&sockAddr, sizeof(sockAddr) );
-        if( bindResult == SOCKET_ERROR )
+        if ( state == parse_state::finished )
         {
-            return false;
+            break;
         }
 
-        const int backlog = 10; //FK: TODO: find reasonable number here
-        const int listenResult = listen(socket, backlog);
-
-        if( listenResult == SOCKET_ERROR )
+        switch ( state )
         {
-            return false;
-        }
-
-        return true;
-    }
-
-    html_client* waitForClientConnection(html_server* pServer)
-    {
-        fd_set readSockets;
-        FD_ZERO(&readSockets);
-        FD_SET(pServer->ipv4Socket, &readSockets);
-        FD_SET(pServer->ipv6Socket, &readSockets);
-
-        const int selectResult = select(0, &readSockets, nullptr, nullptr, nullptr);
-        if (selectResult == -1)
-        {
-            return nullptr;
-        }
-
-        html_client* pClient = (html_client*)malloc(sizeof(html_client));
-        if (FD_ISSET(pServer->ipv4Socket, &readSockets))
-        {
-            pClient->socket = accept(pServer->ipv4Socket, NULL, NULL);
-        }
-        else
-        {
-            pClient->socket = accept(pServer->ipv6Socket, NULL, NULL);
-        }
-
-        return pClient;
-    }
-
-    void parseClientRequest(html_request* pRequest, const char* pMessageBuffer, int messageBufferLength)
-    {
-        //FK: Add state machine
-        const char* pToken = nullptr;
-        int startTokenCharIndex = -1;
-        for( int charIndex = 0; charIndex < messageBufferLength; ++charIndex)
-        {
-            const char messageBufferChar = pMessageBuffer[ charIndex ];
-
-            //FK: eat whitespace
-            if( isAsciiWhiteSpace( messageBufferChar ) )
+        case parse_state::method:
             {
-                continue;
-            }
-
-            if (startTokenCharIndex == -1)
-            {
-              startTokenCharIndex = charIndex;
-            }
-
-            if( ( charIndex + 1) == messageBufferLength )
-            {
-                
-            }
-            else
-            {
-                const char nextMessageBufferChar = pMessageBuffer[ charIndex + 1 ];
-                if( isAsciiWhiteSpace( nextMessageBufferChar ) )
+                if ( compareAsciiStringNonCaseSensitive( pMessageRunningPtr, "get" ) )
                 {
-
+                    request.method = request_method::get;
+                    state          = parse_state::path;
                 }
-            }
-        }
-    }
-
-    result<void> receiveClientData( slice< char >* pMessageBuffer, html_client* pClient)
-    {
-        while( true )
-        {
-            char buffer[256];
-            const int bytesRead = recv( pClient->socket, buffer, 256, 0u );
-
-            if (bytesRead == -1)
-            {
-                return error_id::socket_error;
-            }
-            else if (bytesRead == 0)
-            {
-                pMessageBuffer->pushBack('\0');
-                return error_id::success;
-            }
-
-            char* pMessage = pMessageBuffer->pushBackRange(bytesRead);
-            if (pMessage == nullptr)
-            {
-                return error_id::out_of_memory;
-            }
-
-            copyMemoryNonOverlapping(pMessage, bytesRead, buffer, bytesRead);
-
-            if (bytesRead < sizeof(buffer))
-            {
-                pMessageBuffer->pushBack('\0');
-                return error_id::success;
-            }
-        }
-
-        return error_id::success;
-
-    }
-
-    result<html_request> parseHtmlRequest(slice< char >* pMessageBuffer)
-    {
-        const char* pMessageRunningPtr = pMessageBuffer->getStart();
-        const char* pMessageEnd = pMessageBuffer->getEnd();
-
-        enum class parse_state
-        {
-            method,
-            path,
-            finished
-        };
-
-        html_request request;
-        K15_UNUSED_VARIABLE(request);
-
-        uint32 tokenStartIndex  = 0u;
-        uint32 tokenLength      = 0u;
-        parse_state state = parse_state::method;
-
-        while( true )
-        {
-            if( pMessageRunningPtr == pMessageEnd )
-            { 
-                return error_id::parse_error;
-            }
-
-            switch( state )
-            {
-                case parse_state::finished:
+                else if ( compareAsciiStringNonCaseSensitive( pMessageRunningPtr, "post" ) )
                 {
-                    return request;
+                    request.method = request_method::post;
+                    state          = parse_state::path;
+                }
+                else if ( compareAsciiStringNonCaseSensitive( pMessageRunningPtr, "put" ) )
+                {
+                    request.method = request_method::put;
+                    state          = parse_state::path;
+                }
+                else if ( compareAsciiStringNonCaseSensitive( pMessageRunningPtr, "delete" ) )
+                {
+                    request.method = request_method::del;
+                    state          = parse_state::path;
+                }
+                else
+                {
+                    return error_id::not_supported;
                 }
 
-                case parse_state::method:
-                {
-                    if( compareStringNonCaseSensitive( pMessageRunningPtr, "get" ) )
-                    {
-                        request.method = request_method::get;
-                        state = parse_state::path;
-                    }
-                    else if (compareStringNonCaseSensitive( pMessageRunningPtr, "post" ) )
-                    {
-                        request.method = request_method::post;
-                        state = parse_state::path;
-                    }
-                    else if( compareStringNonCaseSensitive( pMessageRunningPtr, "put" ) )
-                    {
-                        request.method = request_method::put;
-                        state = parse_state::path;
-                    }
-                    else if( compareStringNonCaseSensitive( pMessageRunningPtr, "delete" ) )
-                    {
-                        request.method = request_method::del;
-                        state = parse_state::path;
-                    }
-                    else
-                    {
-                        return error_id::not_supported;
-                    }
+                pMessageRunningPtr = findNextAsciiWhiteSpace( pMessageRunningPtr );
+                ++pMessageRunningPtr;
 
-                    pMessageRunningPtr = getAfterNextAsciiWhiteSpace( pMessageRunningPtr );
-                    
-                    break;
-                }
-          
-                case parse_state::path:
-                {
-                    const char* pPathBegin = pMessageRunningPtr;
-                    const char* pPathEnd = pPathBegin;
-
-                    while( isAsciiWhiteSpace( *pPathEnd++ ) )
-                    {
-                        if( pPathEnd >= pMessageEnd )
-                        {
-                            return error_id::parse_error;
-                        }
-                    }
-
-                    request.path = string_view( pPathBegin, pPathEnd - pPathBegin );
-                    state = parse_state::finished;
-                    break;
-                }
+                break;
             }
-        }
 
-        return request;
-    }
-
-    result< html_request > readClientRequest(html_client* pClient)
-    {
-        //html_request request;
-        //dynamic_array< char, 256u > messageBuffer;
-        dynamic_array< char > messageBuffer;
-        const result< void > receiveResult = receiveClientData(&messageBuffer, pClient);
-        if (receiveResult.hasError())
-        {
-            return receiveResult.getError();
-        }
-
-       return parseHtmlRequest( &messageBuffer );
-    }
-
-    result<html_server*> createHtmlServer(const html_server_parameters& parameters)
-    {
-        //FK: TODO: use own allocation strategy
-        html_server* pServer = (html_server*)malloc(sizeof(html_server));
-        pServer->ipv4Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	    pServer->ipv6Socket = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-
-	    if (pServer->ipv4Socket == INVALID_SOCKET && pServer->ipv6Socket == INVALID_SOCKET)
-	    {
-	    	return error_id::socket_error;	
-	    }
-
-	    if (!listenOnSocket(pServer->ipv4Socket, AF_INET, parameters.port, parameters.ipv4BindAddress) && ! listenOnSocket(pServer->ipv4Socket, AF_INET6, parameters.port, parameters.ipv6BindAddress) )
-        {
-            return error_id::listen_error;
-        }
-
-        return pServer;
-    }
-
-    bool serveHtmlClients(html_server* pServer)
-    {
-        while( true )
-        {
-            html_client* pClient = waitForClientConnection(pServer);
-            if (pClient == nullptr)
+        case parse_state::path:
             {
-                continue;
-            }
+                const char* pPathBegin = pMessageRunningPtr;
+                const char* pPathEnd   = findNextAsciiWhiteSpace( pPathBegin );
+                if ( pPathEnd == pPathBegin )
+                {
+                    return error_id::parse_error;
+                }
 
-            const result<html_request> requestResult = readClientRequest(pClient);
-            const html_request& request = requestResult.getValue();
-            K15_UNUSED_VARIABLE(request);
+                request.path = string_view( pPathBegin, pPathEnd );
+                state        = parse_state::finished;
+                break;
+            }
         }
+    }
+
+    request.message.swapFrom( pMessageBuffer );
+
+    return request;
+}
+
+result< html_request > readClientRequest( html_client* pClient )
+{
+    //html_request request;
+    //dynamic_array< char, 256u > messageBuffer;
+    dynamic_array< char > messageBuffer( pClient->pAllocator );
+    const result< void >  receiveResult = receiveClientData( &messageBuffer, pClient );
+    if ( receiveResult.hasError() )
+    {
+        return receiveResult.getError();
+    }
+
+    return parseHtmlRequest( &messageBuffer );
+}
+
+void destroyHtmlServer( html_server* pServer )
+{
+    if ( pServer->ipv4Socket != INVALID_SOCKET )
+    {
+        closesocket( pServer->ipv4Socket );
+        pServer->ipv4Socket = INVALID_SOCKET;
+    }
+
+    if ( pServer->ipv6Socket != INVALID_SOCKET )
+    {
+        closesocket( pServer->ipv6Socket );
+        pServer->ipv6Socket = INVALID_SOCKET;
+    }
+
+    deleteObject( pServer, pServer->pAllocator );
+}
+
+result< html_server* > createHtmlServer( const html_server_parameters& parameters )
+{
+    K15_ASSERT( parameters.pAllocator != nullptr );
+    memory_allocator* pAllocator = parameters.pAllocator;
+
+    html_server* pServer = newObject< html_server >( pAllocator );
+    pServer->ipv4Socket  = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+    pServer->ipv6Socket  = socket( AF_INET6, SOCK_STREAM, IPPROTO_TCP );
+    pServer->port        = parameters.port;
+    pServer->pAllocator  = pAllocator;
+
+    if ( pServer->ipv4Socket == INVALID_SOCKET && pServer->ipv6Socket == INVALID_SOCKET )
+    {
+        destroyHtmlServer( pServer );
+        return error_id::socket_error;
+    }
+
+    if ( !listenOnSocket( pServer->ipv4Socket, AF_INET, parameters.port, parameters.ipv4BindAddress ) && !listenOnSocket( pServer->ipv4Socket, AF_INET6, parameters.port, parameters.ipv6BindAddress ) )
+    {
+        destroyHtmlServer( pServer );
+        return error_id::listen_error;
+    }
+
+    return pServer;
+}
+
+bool serveHtmlClients( html_server* pServer )
+{
+    while ( true )
+    {
+        html_client* pClient = waitForClientConnection( pServer );
+        if ( pClient == nullptr )
+        {
+            continue;
+        }
+
+        const result< html_request > requestResult = readClientRequest( pClient );
+        const html_request&          request       = requestResult.getValue();
+        K15_UNUSED_VARIABLE( request );
     }
 }
+} // namespace k15
 
 #endif //K15_HTML_SERVER_INCLUDE
--- k15_server_manager.cpp
+++ k15_server_manager.cpp
@@ -18,197 +18,191 @@
 #include "k15_std/src/k15_platform_win32.cpp"
 #include "k15_std/src/k15_profiling_win32.cpp"
 
-#pragma comment(lib, "kernel32.lib")
-#pragma comment(lib, "user32.lib")
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment( lib, "kernel32.lib" )
+#pragma comment( lib, "user32.lib" )
+#pragma comment( lib, "ws2_32.lib" )
 
 #define K15_FALSE 0
-#define K15_TRUE 1
+#define K15_TRUE  1
 
 using namespace k15;
 
-typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
+typedef LRESULT( CALLBACK* WNDPROC )( HWND, UINT, WPARAM, LPARAM );
 
-void printErrorToFile(const char* p_FileName)
+void printErrorToFile( const char* p_FileName )
 {
-	DWORD errorId = GetLastError();
-	char* textBuffer = 0;
-	DWORD writtenChars = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, errorId, 
-		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), (LPSTR)&textBuffer, 512, 0);
+    DWORD errorId      = GetLastError();
+    char* textBuffer   = 0;
+    DWORD writtenChars = FormatMessage( FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, 0, errorId,
+                                        MAKELANGID( LANG_ENGLISH, SUBLANG_ENGLISH_US ), ( LPSTR )&textBuffer, 512, 0 );
 
-	if (writtenChars > 0)
-	{
-		FILE* file = fopen(p_FileName, "w");
+    if ( writtenChars > 0 )
+    {
+        FILE* file = fopen( p_FileName, "w" );
 
-		if (file)
-		{
-			fwrite(textBuffer, writtenChars, 1, file);			
-			fflush(file);
-			fclose(file);
-		}
-	}
+        if ( file )
+        {
+            fwrite( textBuffer, writtenChars, 1, file );
+            fflush( file );
+            fclose( file );
+        }
+    }
 }
 
 void allocateDebugConsole()
 {
-	AllocConsole();
-	AttachConsole(ATTACH_PARENT_PROCESS);
-	freopen("CONOUT$", "w", stdout);
+    AllocConsole();
+    AttachConsole( ATTACH_PARENT_PROCESS );
+    freopen( "CONOUT$", "w", stdout );
 }
 
-void K15_WindowCreated(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_WindowCreated( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-void K15_WindowClosed(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_WindowClosed( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-void K15_KeyInput(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_KeyInput( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-void K15_MouseButtonInput(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_MouseButtonInput( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-void K15_MouseMove(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_MouseMove( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-void K15_MouseWheel(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+void K15_MouseWheel( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-
 }
 
-LRESULT CALLBACK K15_WNDPROC(HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam)
+LRESULT CALLBACK K15_WNDPROC( HWND p_HWND, UINT p_Message, WPARAM p_wParam, LPARAM p_lParam )
 {
-	bool8 messageHandled = K15_FALSE;
+    bool8 messageHandled = K15_FALSE;
 
-	switch (p_Message)
-	{
-	case WM_CREATE:
-		K15_WindowCreated(p_HWND, p_Message, p_wParam, p_lParam);
-		break;
+    switch ( p_Message )
+    {
+    case WM_CREATE:
+        K15_WindowCreated( p_HWND, p_Message, p_wParam, p_lParam );
+        break;
 
-	case WM_CLOSE:
-		K15_WindowClosed(p_HWND, p_Message, p_wParam, p_lParam);
-		PostQuitMessage(0);
-		messageHandled = K15_TRUE;
-		break;
+    case WM_CLOSE:
+        K15_WindowClosed( p_HWND, p_Message, p_wParam, p_lParam );
+        PostQuitMessage( 0 );
+        messageHandled = K15_TRUE;
+        break;
 
-	case WM_KEYDOWN:
-	case WM_KEYUP:
-	case WM_SYSKEYDOWN:
-	case WM_SYSKEYUP:
-		K15_KeyInput(p_HWND, p_Message, p_wParam, p_lParam);
-		break;
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    case WM_SYSKEYDOWN:
+    case WM_SYSKEYUP:
+        K15_KeyInput( p_HWND, p_Message, p_wParam, p_lParam );
+        break;
 
-	case WM_LBUTTONUP:
-	case WM_MBUTTONUP:
-	case WM_RBUTTONUP:
-	case WM_XBUTTONUP:
-	case WM_LBUTTONDOWN:
-	case WM_RBUTTONDOWN:
-	case WM_MBUTTONDOWN:
-	case WM_XBUTTONDOWN:
-		K15_MouseButtonInput(p_HWND, p_Message, p_wParam, p_lParam);
-		break;
+    case WM_LBUTTONUP:
+    case WM_MBUTTONUP:
+    case WM_RBUTTONUP:
+    case WM_XBUTTONUP:
+    case WM_LBUTTONDOWN:
+    case WM_RBUTTONDOWN:
+    case WM_MBUTTONDOWN:
+    case WM_XBUTTONDOWN:
+        K15_MouseButtonInput( p_HWND, p_Message, p_wParam, p_lParam );
+        break;
 
-	case WM_MOUSEMOVE:
-		K15_MouseMove(p_HWND, p_Message, p_wParam, p_lParam);
-		break;
+    case WM_MOUSEMOVE:
+        K15_MouseMove( p_HWND, p_Message, p_wParam, p_lParam );
+        break;
 
-	case WM_MOUSEWHEEL:
-		K15_MouseWheel(p_HWND, p_Message, p_wParam, p_lParam);
-		break;
-	}
+    case WM_MOUSEWHEEL:
+        K15_MouseWheel( p_HWND, p_Message, p_wParam, p_lParam );
+        break;
+    }
 
-	if (messageHandled == K15_FALSE)
-	{
-		return DefWindowProc(p_HWND, p_Message, p_wParam, p_lParam);
-	}
+    if ( messageHandled == K15_FALSE )
+    {
+        return DefWindowProc( p_HWND, p_Message, p_wParam, p_lParam );
+    }
 
-	return 0;
+    return 0;
 }
 
-HWND setupWindow(HINSTANCE p_Instance, int p_Width, int p_Height)
+HWND setupWindow( HINSTANCE p_Instance, int p_Width, int p_Height )
 {
-	WNDCLASS wndClass = {0};
-	wndClass.style = CS_HREDRAW | CS_OWNDC | CS_VREDRAW;
-	wndClass.hInstance = p_Instance;
-	wndClass.lpszClassName = "K15_Win32Template";
-	wndClass.lpfnWndProc = K15_WNDPROC;
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	RegisterClass(&wndClass);
+    WNDCLASS wndClass      = { 0 };
+    wndClass.style         = CS_HREDRAW | CS_OWNDC | CS_VREDRAW;
+    wndClass.hInstance     = p_Instance;
+    wndClass.lpszClassName = "K15_Win32Template";
+    wndClass.lpfnWndProc   = K15_WNDPROC;
+    wndClass.hCursor       = LoadCursor( NULL, IDC_ARROW );
+    RegisterClass( &wndClass );
 
-	HWND hwnd = CreateWindowA("K15_Win32Template", "Win32 Template",
-		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-		p_Width, p_Height, 0, 0, p_Instance, 0);
+    HWND hwnd = CreateWindowA( "K15_Win32Template", "Win32 Template",
+                               WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                               p_Width, p_Height, 0, 0, p_Instance, 0 );
 
-	if (hwnd == INVALID_HANDLE_VALUE)
-		MessageBox(0, "Error creating Window.\n", "Error!", 0);
-	else
-		ShowWindow(hwnd, SW_SHOW);
-	return hwnd;
+    if ( hwnd == INVALID_HANDLE_VALUE )
+        MessageBox( 0, "Error creating Window.\n", "Error!", 0 );
+    else
+        ShowWindow( hwnd, SW_SHOW );
+    return hwnd;
 }
 
-uint32 getTimeInMilliseconds(LARGE_INTEGER p_PerformanceFrequency)
+uint32 getTimeInMilliseconds( LARGE_INTEGER p_PerformanceFrequency )
 {
-	LARGE_INTEGER appTime = {0};
-	QueryPerformanceFrequency(&appTime);
+    LARGE_INTEGER appTime = { 0 };
+    QueryPerformanceFrequency( &appTime );
 
-	appTime.QuadPart *= 1000; //to milliseconds
+    appTime.QuadPart *= 1000; //to milliseconds
 
-	return (uint32)(appTime.QuadPart / p_PerformanceFrequency.QuadPart);
+    return ( uint32 )( appTime.QuadPart / p_PerformanceFrequency.QuadPart );
 }
 
 void setup()
 {
-	allocateDebugConsole();
+    allocateDebugConsole();
 }
 
-void doFrame(uint32 p_DeltaTimeInMS)
+void doFrame( uint32 p_DeltaTimeInMS )
 {
-
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nShowCmd)
+int CALLBACK WinMain( HINSTANCE hInstance,
+                      HINSTANCE hPrevInstance,
+                      LPSTR lpCmdLine, int nShowCmd )
 {
-	LARGE_INTEGER performanceFrequency;
-	QueryPerformanceFrequency(&performanceFrequency);
+    LARGE_INTEGER performanceFrequency;
+    QueryPerformanceFrequency( &performanceFrequency );
 
-	//FK: TODO: WSAGetLastError error handling
-	setup();
+    //FK: TODO: WSAGetLastError error handling
+    setup();
 
-	WSADATA wsa;
-	if( WSAStartup(MAKEWORD(2,2), &wsa) != 0)
-	{
-		printf("WSAStartup failed...\n");
-		return -1;
-	}
+    WSADATA wsa;
+    if ( WSAStartup( MAKEWORD( 2, 2 ), &wsa ) != 0 )
+    {
+        printf( "WSAStartup failed...\n" );
+        return -1;
+    }
 
-	html_server_parameters parameters;
-	parameters.port = 9090;
-	parameters.ipv4BindAddress = "0.0.0.0";
-	parameters.ipv6BindAddress = "::0";
+    html_server_parameters parameters;
+    parameters.port            = 9090;
+    parameters.ipv4BindAddress = "0.0.0.0";
+    parameters.ipv6BindAddress = "::0";
+    parameters.pAllocator      = getCrtMemoryAllocator();
 
-	result<html_server*> initResult = createHtmlServer(parameters);
-	if( initResult.hasError() )
-	{
-		printf("Couldn't initialize html server.\n");
-		return -1;
-	}
+    result< html_server* > initResult = createHtmlServer( parameters );
+    if ( initResult.hasError() )
+    {
+        printf( "Couldn't initialize html server.\n" );
+        return -1;
+    }
 
-	html_server* pServer = initResult.getValue();
-	return serveHtmlClients( pServer );
+    html_server* pServer = initResult.getValue();
+    return serveHtmlClients( pServer );
 
-	return 0;
+    return 0;
 }
